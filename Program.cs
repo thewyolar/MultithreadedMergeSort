6ff3730327d34aaccba99d259c3f9da5d8a9ca9c
thewyolar <thewyolar@gmail.com>
2022-02-22 22:43:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MultithreadedMergeSort
+{
+    class Program
+    {
+        public static void Test(int length)
+        {
+            Console.WriteLine();
+
+            int[] array1 = new int[length];
+            Generate(array1);
+            int[] array2 = new int[array1.Length];
+            Array.Copy(array1, array2, array1.Length);
+
+            Console.WriteLine($"Тест. Кол-во элементов {array1.Length}:");
+            Stopwatch sw = new Stopwatch();
+
+            SimpleMerger simpleMerger = new SimpleMerger(array1);
+            sw.Start();
+            simpleMerger.MergeSort();
+            sw.Stop();
+            Console.WriteLine("[Merge Sort] Время, затраченное на выполнение: " + sw.ElapsedMilliseconds + "ms");
+
+            MultiMerger multiMerger = new MultiMerger(array2);
+            /*for (int i = 1; i < 13; i++)
+            {
+                sw.Restart();
+                multiMerger.MergeSortMT(i);
+                sw.Stop();
+                Console.WriteLine($"Кол-во потоков: {i}");
+                Console.WriteLine("[Multithreading Merge Sort] Время, затраченное на выполнение: " + sw.ElapsedMilliseconds + "ms");
+            }*/
+            sw.Restart();
+            multiMerger.MergeSortMT(6);
+            sw.Stop();
+            Console.WriteLine("[Multithreading Merge Sort] Время, затраченное на выполнение: " + sw.ElapsedMilliseconds + "ms");
+        }
+
+        public static void Generate(int[] array)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(0, 10000);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск тестов.");
+
+            Test(1000);
+            Test(10000);
+            Test(100000);
+            Test(1000000);
+            Test(10000000);
+
+            Console.WriteLine();
+            Console.WriteLine("Для выхода нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpleMerger.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadedMergeSort
+{
+    class SimpleMerger
+    {
+        private int[] sorted;
+
+        public SimpleMerger(int[] unsorted)
+        {
+            this.sorted = unsorted;
+        }
+
+        public int[] Sorted() => this.sorted;
+
+        //метод для слияния массивов
+        public void Merge(int lowIndex, int middleIndex, int highIndex)
+        {
+            int left = lowIndex;
+            int right = middleIndex + 1;
+            int[] tempArray = new int[highIndex - lowIndex + 1];
+            int index = 0;
+
+            while (left <= middleIndex && right <= highIndex)
+            {
+                if (sorted[left] < sorted[right])
+                {
+                    tempArray[index] = sorted[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = sorted[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            for (int i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = sorted[i];
+                index++;
+            }
+
+            for (int i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = sorted[i];
+                index++;
+            }
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                sorted[lowIndex + i] = tempArray[i];
+            }
+        }
+
+        //однопоточная сортировка слиянием
+        public void MergeSort(int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                int middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(lowIndex, middleIndex);
+                MergeSort(middleIndex + 1, highIndex);
+                Merge(lowIndex, middleIndex, highIndex);
+            }
+        }
+
+        public void MergeSort()
+        {
+            MergeSort(0, sorted.Length - 1);
+        }
+    }
+}
